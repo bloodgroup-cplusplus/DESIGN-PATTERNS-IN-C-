fc68dc4b991372d8b50b885a007c1dd52d48e17e
Bhushan <tucater10896601@protonmail.com>
2022-05-06 14:38:26 +0530

    added three files under the project

---
+++ memlogger/src/free_memory_api/free_memory_api.c
@@ -0,0 +1,18 @@
+#include<sys/sysinfo.h>
+#include "free_memory_api.h"
+
+unsigned long long get_free_system_memory()
+{
+    struct sysinfo info;
+
+    if(sysinfo(&info) <0)
+    {
+        return 0;
+    }
+
+
+    return info.freeram;
+
+}
+
+
