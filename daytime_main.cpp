925ed26187e62255b0969ac478e0fe28a7002711
Bhushan <tucater10896601@protonmail.com>
2022-06-04 16:14:37 +0530

    added  main file for the module  or daytime.cppm file

---
+++ daytime_main.cpp
@@ -0,0 +1,19 @@
+import daytime;
+
+#include<iostream>
+
+int main(int argc, char *argv[])
+{
+    auto client = daytime::client();
+    std::cout<<client.get_value()<<std::endl;
+    auto helper = daytime::helper();
+    return 0;
+}
+
+
+// when something is exported it is visible 
+// When it is exported it is available 
+// Reachability is a subset fo visibility 
+// Semantics is available
+// Size and type id is visible to us 
+//
