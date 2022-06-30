269e9a0f933c373d3273bd8e90d115b7836f5c88
Bhushan <tucater10896601@protonmail.com>
2022-06-30 22:33:57 +0530

    the main file

---
+++ SimpleAdder/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "LibraryCode.hpp"
+
+int main(int argc, char **argv)
+{
+    int s = sums(2,3);
+    std::cout<<"Actual Appication\n "<<std::endl;
+    std::cout<<"Sum = "<<s <<std::endl;
+    return 0;
+
+}
